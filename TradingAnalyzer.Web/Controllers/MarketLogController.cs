447e8fec7ae749726116e3f0474c3bbdbc6a89e0
calebsandfort <caleb.sandfort@gmail.com>
2017-07-11 22:42:44 -0700

    MarketLog_Read

--- TradingAnalyzer.Web/Controllers/MarketLogController.cs
+++ TradingAnalyzer.Web/Controllers/MarketLogController.cs
@@ -1,17 +1,51 @@
-﻿using System;
+﻿using Abp.Domain.Repositories;
+using Abp.ObjectMapping;
+using Kendo.Mvc.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
+using TradingAnalyzer.Entities;
+using TradingAnalyzer.Entities.Dtos;
+using TradingAnalyzer.Web.Framework;
 
 namespace TradingAnalyzer.Web.Controllers
 {
     public class MarketLogController : TradingAnalyzerControllerBase
     {
+        readonly IRepository<MarketLogEntry> _marketLogEntryRepository;
+        readonly IObjectMapper _objectMapper;
+
+        public MarketLogController(IRepository<MarketLogEntry> marketLogEntryRepository, IObjectMapper objectMapper)
+        {
+            _marketLogEntryRepository = marketLogEntryRepository;
+            _objectMapper = objectMapper;
+        }
+
         // GET: Log
         public ActionResult Index()
         {
             return View();
         }
+
+        #region MarketLog_Read
+        public ActionResult MarketLog_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            DataSourceResult result = new DataSourceResult();
+
+            //DateTime currentDate = Clock.Now;
+
+            //Expression<Func<NbaGame, bool>> todayFunc = x => x.Date.Year == currentDate.Year && x.Date.Month == currentDate.Month && x.Date.Day == currentDate.Day;
+
+            //result.Data = _objectMapper.Map<List<MarketLogEntryDto>>(_movieRepository.GetAllIncluding(x => x.StatLines.Select(y => y.Participant)).Where(request.Filters).Where(todayFunc).OrderBy(request.Sorts[0]).ToList());
+            //result.Total = _marketLogEntryRepository.GetAll().Where(request.Filters).Where(todayFunc).Count();
+
+            result.Data = _objectMapper.Map<List<MarketLogEntryDto>>(_marketLogEntryRepository.GetAll().OrderByDescending(x => x.TimeStamp).ToList());
+
+            return new GuerillaLogisticsApiJsonResult(result);
+        }
+        #endregion
     }
 }
